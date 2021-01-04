e254b4c1dc60f62ca3ad316e3220ce156b5d2e53
raultg <raul.torres.garcia@alumnos.upm.es>
2021-01-04 01:11:42 +0100

    Creada ExtNet en init.sh. Siguiente paso definir scenario.yaml

--- init.sh
+++ init.sh
@@ -12,9 +12,6 @@
 sudo vnx -f /mnt/tmp/openstack_lab-stein_4n_classic_ovs-v06/openstack_lab.xml --create 
 sudo vnx -f /mnt/tmp/openstack_lab-stein_4n_classic_ovs-v06/openstack_lab.xml -x start-all,load-img
 
-#CONFIGURE NAT
-EXT=`ifconfig | grep enp | awk '{print $1}' | tr -d ':'`
-sudo vnx_config_nat ExtNet $EXT
 
 #CREATE ADMIN USER
 #USAMOS INICIALMENTE PRIVILEGIOS DE USUARIO ADMIN PARA CREAR NUESTRO PROPIO USUARIO
@@ -25,3 +22,10 @@ openstack role add --project group1project --user group1user admin
 
 #Usuario: group1user | Password: xxxx
 source bin/group1user-openrc.sh
+
+#ExtNet (Antes se creaba con create-demo-scenario desde VNX, ahora todo eso se hace con Heat)
+openstack network create --share --external --provider-physical-network provider --provider-network-type flat ExtNet
+openstack subnet create --network ExtNet --gateway 10.0.10.1 --dns-nameserver 10.0.10.1 --subnet-range 10.0.10.0/24 --allocation-pool start=10.0.10.100,end=10.0.10.200 ExtSubNet
+#CONFIGURE NAT
+EXT=`ifconfig | grep enp | awk '{print $1}' | tr -d ':'`
+sudo vnx_config_nat ExtNet $EXT
