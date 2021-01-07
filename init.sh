2b1982b838448ede77285bbdbf1a52eea2f69e83
raultg <raul.torres.garcia@alumnos.upm.es>
2021-01-07 15:50:15 +0100

    Cambios menores scenario.yaml

--- init.sh
+++ init.sh
@@ -8,6 +8,14 @@ sudo vnx -f /mnt/tmp/openstack_lab-stein_4n_classic_ovs-v06/openstack_lab.xml -v
 sudo vnx -f /mnt/tmp/openstack_lab-stein_4n_classic_ovs-v06/openstack_lab.xml -v -x start-all
 sudo vnx -f /mnt/tmp/openstack_lab-stein_4n_classic_ovs-v06/openstack_lab.xml -v -x load-img
 
+#CONFIGURE NAT
+EXT=`ifconfig | grep enp | awk '{print $1}' | tr -d ':'`
+sudo vnx_config_nat ExtNet $EXT
+
+#ExtNet (Antes se creaba con create-demo-scenario desde VNX, ahora todo eso se hace con Heat)
+#openstack network create --share --external --provider-physical-network provider --provider-network-type flat ExtNet
+#openstack subnet create --network ExtNet --gateway 10.0.10.1 --dns-nameserver 10.0.10.1 --subnet-range 10.0.10.0/24 --allocation-pool start=10.0.10.100,end=10.0.10.200 ExtSubNet
+
 
 #CREATE ADMIN USER
 #USAMOS INICIALMENTE PRIVILEGIOS DE USUARIO ADMIN PARA CREAR NUESTRO PROPIO USUARIO
@@ -23,21 +31,17 @@ sleep 30
 #Usuario: group1user | Password: xxxx
 source bin/group1user-openrc.sh
 
-#ExtNet (Antes se creaba con create-demo-scenario desde VNX, ahora todo eso se hace con Heat)
-#openstack network create --share --external --provider-physical-network provider --provider-network-type flat ExtNet
-#openstack subnet create --network ExtNet --gateway 10.0.10.1 --dns-nameserver 10.0.10.1 --subnet-range 10.0.10.0/24 --allocation-pool start=10.0.10.100,end=10.0.10.200 ExtSubNet
-#CONFIGURE NAT
-EXT=`ifconfig | grep enp | awk '{print $1}' | tr -d ':'`
-sudo vnx_config_nat ExtNet $EXT
+
 
 #HEAT STACK
 openstack stack create -t scenario.yaml --parameter "key_name=key_group1" stackscenario
 
 
+#LOAD BALANCER
+
 #FIREWALL
 #./firewallConfiguration.sh
 
-#LOAD BALANCER
 
 
 
