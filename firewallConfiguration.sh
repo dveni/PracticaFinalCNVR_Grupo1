cb53127d6de63434a8454b32fb82557aa2062eba
sfereres <71426971+sfereres@users.noreply.github.com>
2021-01-08 17:47:47 +0100

    Update firewallConfiguration.sh

--- firewallConfiguration.sh
+++ firewallConfiguration.sh
@@ -3,8 +3,12 @@
 
 source bin/group1user-openrc.sh
 
-#Permitir acceso desde el exterior al puerto SSH(22) de s_admin (10.1.1.97)
+# IPs
 IP_SADMIN="$(openstack stack output show stackscenario s_admin_private_ip_net1 -c output_value -f value)"
+IP_LB="$(neutron lbaas-loadbalancer-show lb | awk 'FNR == 14 {print $4}')"
+PORT_ID="$(openstack port list --fixed-ip subnet=subnet1,ip-address=10.1.1.1 -c ID -f value)"
+
+#Permitir acceso desde el exterior al puerto SSH(22) de s_admin (10.1.1.97)
 openstack firewall group rule create --protocol tcp --destination-port 22 --destination-ip-address "${IP_SADMIN}" --action allow --name fw_rule_sadmin
 
 #Permitir acceso desde el interior (10.1.1.0/24 - 10.1.2.0/24) a cualquier destino con cualquier protocolo
@@ -12,7 +16,7 @@ openstack firewall group rule create --protocol any --source-ip-address 10.1.1.0
 openstack firewall group rule create --protocol any --source-ip-address 10.1.2.0/24 --destination-ip-address 0.0.0.0/24 --action allow --name fw_rule_net2
 
 #Permitir acceso desde el exterior al puerto WWW(80) del Balanceador de carga
-openstack firewall group rule create --protocol tcp --destination-port 80 --destination-ip-address "${IP_balanceador}" --action allow --name fw_rule_lb
+openstack firewall group rule create --protocol tcp --destination-port 80 --destination-ip-address "${IP_LB}" --action allow --name fw_rule_lb
 
 #Crear el firewall policy y añadir las reglas al policy
 openstack firewall group policy create --firewall-rule "fw_rule_sadmin" fwcnvr
@@ -21,12 +25,5 @@ openstack firewall group policy add rule fwcnvr "fw_rule_net2"
 openstack firewall group policy add rule fwcnvr "fw_rule_lb"
 
 #Crear el firewall group
-openstack firewall group create --ingress-firewall-policy "fwcnvr" --no-port
-
-
-
-
-# IPs
-LB="$(neutron lbaas-loadbalancer-show lb | awk 'FNR == 14 {print $4}')"
-# Rules creation
-openstack firewall group rule create --protocol tcp --destination-port 80 --destination-ip-address "${LB}" --action allow --name fw_rule_lb
+#openstack firewall group create --ingress-firewall-policy "fwcnvr" --no-port
+openstack firewall group create --ingress-firewall-policy "fwcnvr" --port "${PORT_ID}"
