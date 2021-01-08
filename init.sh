b1bb278190ec2c4b7ccceb756e19861b273985f6
raultg <raul.torres.garcia@alumnos.upm.es>
2021-01-08 10:52:21 +0100

    Modificado wget

--- init.sh
+++ init.sh
@@ -34,8 +34,8 @@ source bin/group1user-openrc.sh
 
 #IMAGES
 #BBDD
-wget https://www.dropbox.com/sh/0zf8sby5qrxratg/AAAwIVmdnDKpwQOy_8f_6-QXa?dl=0
-mv AAAwIVmdnDKpwQOy_8f_6-QXa?dl=0 bbdd-server.raw
+wget https://www.dropbox.com/s/x8mcmw29uqlo68l/bbdd-server.raw?dl=0
+mv bbdd-server.raw?dl=0 bbdd-server.raw
 glance image-create --name "bbdd-image" --file bbdd-server.raw --disk-format raw --container-format bare --visibility public --progress
 
 #WEBSERVER
