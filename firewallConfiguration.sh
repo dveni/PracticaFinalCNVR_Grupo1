47759ba11aaaeedab447aa90872eb4842c810738
sfereres <71426971+sfereres@users.noreply.github.com>
2021-01-10 20:47:39 +0100

    Update firewallConfiguration.sh
    Se quita dirección ip orígen para ssh

--- firewallConfiguration.sh
+++ firewallConfiguration.sh
@@ -7,10 +7,10 @@ source bin/group1user-openrc.sh
 IP_SADMIN="$(openstack stack output show stackscenario s_admin_private_ip_net1 -c output_value -f value)"
 IP_LB="$(neutron lbaas-loadbalancer-show lb | awk 'FNR == 14 {print $4}')"
 PORT_ID="$(openstack port list --fixed-ip subnet=subnet1,ip-address=10.1.1.1 -c ID -f value)"
-IP_HOST="$(hostname -I | awk '{printf "%s\n", $1}{print $2}' | grep 138.4.31)"
+#IP_HOST="$(hostname -I | awk '{printf "%s\n", $1}{print $2}' | grep 138.4.31)"
 
 #Permitir acceso desde el exterior al puerto SSH(22) de s_admin 
-openstack firewall group rule create --protocol tcp --destination-port 2020 --source-ip-address "${IP_HOST}" --destination-ip-address "${IP_SADMIN}" --action allow --name fw_rule_sadmin
+openstack firewall group rule create --protocol tcp --destination-port 2020 --destination-ip-address "${IP_SADMIN}" --action allow --name fw_rule_sadmin
 
 #Permitir acceso desde el interior (10.1.1.0/24 - 10.1.2.0/24) a cualquier destino con cualquier protocolo
 openstack firewall group rule create --protocol any --source-ip-address 10.1.1.0/24 --action allow --name fw_rule_net1
@@ -18,12 +18,12 @@ openstack firewall group rule create --protocol any --source-ip-address 10.1.2.0
 
 #Permitir acceso desde el exterior al puerto WWW(80) del Balanceador de carga
 openstack firewall group rule create --protocol tcp --destination-port 80 --destination-ip-address "${IP_LB}" --action allow --name fw_rule_lb
-sleep 3
+sleep 5
 
 #Crear los firewall policy y añadir las reglas al policy
 openstack firewall group policy create --firewall-rule fw_rule_sadmin --firewall-rule fw_rule_lb my-ingress-policy
 openstack firewall group policy create --firewall-rule fw_rule_net1 --firewall-rule fw_rule_net2 my-egress-policy
-sleep 3
+sleep 5
 
 #Crear el firewall group
 #openstack firewall group create --ingress-firewall-policy "fwcnvr" --no-port
